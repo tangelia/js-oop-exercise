4d9590bf095525bc8a5ac725bfee7a2ead1a9464
tangelia <tangelia.atlanta@gmail.com>
2019-05-03 15:44:38 -0400

    First commit

---
+++ tamagotchi.js
@@ -0,0 +1,19 @@
+console.log('tamagotchi file is loaded');
+
+//your class declaration here
+class Tamagotchis {
+    constructor(){
+        this.foodInTummy = 10;
+        this.restedness= 10;
+        this.health = 10;
+    }
+
+//create new Tamagotchis
+
+
+//test out your Tamagotchies below via console.logs
+
+
+
+
+    
