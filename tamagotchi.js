508558691bbe2aa51f28f31bf4775332aecc7f50
tangelia <tangelia.atlanta@gmail.com>
2019-05-05 06:09:43 -0400

    part 3: added new methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -19,6 +19,15 @@ class Tamagotchi {
     this.creatureType+ " " + this.name;
     console.log("I am a "+this.creatureType+" and my name is "+this.name);
 }
+puke(){
+    this.foodInTummy --; 
+    console.log("WAHH!!!");
+    console.log('Oh no! '+this.name+ ' is sick. Food in tummy: ' +this.foodInTummy);
+}
+yawn(){
+    this.restedness --; 
+    console.log (this.name+"  has current restedness of: "+this.restedness)
+}
 }
 //create new Tamagotchis
 
@@ -26,21 +35,24 @@ let newToy1 = new Tamagotchi()
 let newToy2 = new Tamagotchi()
 let newToy3 = new Tamagotchi("Devil","Tommy")
 let newToy4 = new Tamagotchi("Angel","Tina")
-
-
+let newToy5 = new Tamagotchi("Brownie","Joseph")
+let newToy6 = new Tamagotchi("Imp","Charles")
 
 
 //test out your Tamagotchies below via console.logs
 
-console.log(newToy1);
-console.log(newToy2);
-console.log(newToy3);
-console.log(newToy4);
+// console.log(newToy1);
+// console.log(newToy2);
+// console.log(newToy3);
+// console.log(newToy4);
+
 
 newToy1.cry();
 newToy2.cry();
 newToy3.describe();
 newToy4.describe();
+newToy5.yawn();
+newToy6.puke();
 
 
 
